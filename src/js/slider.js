b9e7946d6825727c6eac8494b6c2e38302888a32
Dostoevsky <79225555735@ya.ru>
2016-05-03 22:22:42 +0500

    add blok youget

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,42 +1,46 @@
-/*
- * Формируем массив данных для слайдера
- */
-var result = {};
-result.slides = [];
+function slider_init() {
+  /*
+  * Формируем массив данных для слайдера
+  */
+  var result = {};
+  result.slides = [];
 
-$('.result__item').each(function(index, el) {
-  
-  var title =       $(el).find('.result__title').text();  
-  var description = $(el).find('.result__desc').text();  
-  var link =        $(el).find('.result__more').attr('href');
+  $('.result__item').each(function(index, el) {
+    
+    var title =       $(el).find('.result__title').text();  
+    var description = $(el).find('.result__desc').text();  
+    var link =        $(el).find('.result__more').attr('href');
 
-  var logo =        $(el)
-                      .find('.result__logo')
-                      .attr('class')
-                      .match(/result__logo_([\S]*)/)[1];
+    var logo =        $(el)
+                        .find('.result__logo')
+                        .attr('class')
+                        .match(/result__logo_([\S]*)/)[1];
 
-  result.slides[index] = {
-    title: title,
-    description: description,
-    link: link,
-    logo: logo
-  }
-});
+    result.slides[index] = {
+      title: title,
+      description: description,
+      link: link,
+      logo: logo
+    }
+  });
 
-var template = $('#result-slider-template').html();
-var compiledTemplate = Template7.compile(template);
-var html = compiledTemplate(result);
-$('#result-slider-wrapper').append(html);
+  var template = $('#result-slider-template').html();
+  var compiledTemplate = Template7.compile(template);
+  var html = compiledTemplate(result);
+  $('#result-slider-wrapper').append(html);
 
 
 
-/*
- * Инициализируем сладер
- */ 
-$('#result-slider').swiper({
+  /*
+  * Инициализируем сладер
+  */ 
+  $('#result-slider').swiper({
 
-  loop: true,
-  pagination: '.swiper-pagination',
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev'
-});
+    loop: true,
+    pagination: '#result-slider .swiper-pagination',
+    nextButton: '#result-slider .swiper-button-next',
+    prevButton: '#result-slider .swiper-button-prev'
+  });
+}
+
+if($('#result-slider').length) slider_init();
--- src/js/slider2.js
+++ src/js/slider2.js
@@ -1,38 +1,40 @@
-/*
- * Формируем массив данных для слайдера
- */
-var youget = {};
-youget.slides = [];
+function slider2_init() {
+  /*
+  * Формируем массив данных для слайдера
+  */
+  var youget = {};
+  youget.slides = [];
 
-$('.youget__item').each(function(index, el) {
-  var title =       $(el).find('.youget__title').text();
-  var logo =        $(el)
-                      .find('.youget__inner')
-                      .attr('class')
-                      .match(/youget__inner_([\S]*)/)[1];
+  $('.youget__item').each(function(index, el) {
+    var title =       $(el).find('.youget__title').html();
+    var logo =        $(el)
+                        .find('.youget__inner')
+                        .attr('class')
+                        .match(/youget__inner_([\S]*)/)[1];
 
-  youget.slides[index] = {
-    title: title,
-    description: description,
-    link: link,
-    logo: logo
-  }
-});
+    youget.slides[index] = {
+      title: title,
+      logo: logo
+    }
+  });
 
-var template = $('#youget-slider-template').html();
-var compiledTemplate = Template7.compile(template);
-var html = compiledTemplate(youget);
-$('#youget-slider-wrapper').append(html);
-
+  var template = $('#youget-slider-template').html();
+  var compiledTemplate = Template7.compile(template);
+  var html = compiledTemplate(youget);
+  $('#youget-slider-wrapper').append(html);
 
 
-/*
- * Инициализируем сладер
- */ 
-$('#youget-slider').swiper({
 
-  loop: true,
-  pagination: '.swiper-pagination',
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev'
-});
+  /*
+  * Инициализируем сладер
+  */ 
+  $('#youget-slider').swiper({
+
+    loop: true,
+    pagination: '#youget-slider .swiper-pagination',
+    nextButton: '#youget-slider .swiper-button-next',
+    prevButton: '#youget-slider .swiper-button-prev'
+  });
+}
+
+if($('#youget-slider').length) slider2_init();
